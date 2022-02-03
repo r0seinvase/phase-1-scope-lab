591017acb5cfe3306e1a2446378417aa293281d5
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-02-03 12:32:09 -0600

    commit tree 69eb8e88114132e0875e7c5cfdd384a9f0a1cdcc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; 
+}
+
+const leastFavoriteCustomer = "John";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "who";
+}
+
